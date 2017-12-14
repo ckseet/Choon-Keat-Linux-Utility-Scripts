7ad18800f67053765b09da6a838694ea2d9ca355
Choon Keat <ckseet@seresoft.com.sg>
2017-12-14 10:27:39 -0700

    Fixed .git to literal string .git 	modified:   syncrepos.sh

--- codecontrol/syncrepos.sh
+++ codecontrol/syncrepos.sh
@@ -12,13 +12,9 @@ function  printUsage
 	echo -e "Usage : $0 PATH_TO_GITREPO_CSV" 
 	echo -e "\tPATH_TO_GITREPO_CSV : path to csv file containing git details"
 	echo -e "\nExpected Git Config CSV layout"
-	echo -e "REPO_FLAG,REMOTE_CONFIG,CURRENT_USER,PATH_TO_LOCAL_REPO"
-	echo -e "\tREPO_FLAG : b|g , b for bitbucket.org, g for github.com"
+	echo -e "REMOTE_CONFIG,CURRENT_USER,PATH_TO_LOCAL_REPO"
 	echo -e "\tREMOTE_CONFIG : Path to git bitbucket config"
-	echo -e "\tCURRENT_USER : User account to sync repositories"
 	echo -e "\tPATH_TO_LOCAL_REPO: Path to current repo"
-	# echo -e "\tREMOTE_OWNER : Owner of repository"
-	#echo -e "\tREPO_NAME : This is the LOCAL Directoryname - Needs to Be contiguous and no special characters"
 }
 
 # Error Checking
@@ -33,7 +29,7 @@ YES=1
 NO=0
 DEBUG_FLAG=$YES
 
-EXPECTED_ENTRY_COLUMNS=4
+EXPECTED_ENTRY_COLUMNS=2
 ERROR_FLAG_REPO_ENTRY="ERROR_REPO_FORMAT"
 ERROR_FLAG_REPO="E";
 
@@ -55,22 +51,18 @@ do
 		#EXIT_MARKER
 	fi
 
-	repoFlag=`echo $line | awk '{split($0,lineArray, ","); print lineArray[1]; }'`
-	remoteConfig=`echo $line | awk '{split($0,lineArray, ","); print lineArray[2]; }'`
-	currentUser=`echo $line | awk '{split($0,lineArray, ","); print lineArray[3]; }'`
-	pathToLocalRepo=`echo $line | awk '{split($0,lineArray, ","); print lineArray[4]; }'`
-	gitFldr=`echo $remoteConfig | awk '{split($0,urlArray, "\/"); print urlArray[length(urlArray)];}' | awk '{split($0,nameArray,".git"); print nameArray[1];}'`
+	remoteConfig=`echo $line | awk '{split($0,lineArray, ","); print lineArray[1]; }'`
+	pathToLocalRepo=`echo $line | awk '{split($0,lineArray, ","); print lineArray[2]; }'`
+	gitFldr=`echo $remoteConfig | awk '{split($0,urlArray, "\/"); print urlArray[length(urlArray)];}' | awk '{split($0,nameArray,"\\\.git"); print nameArray[1];}'`
 	# Only git functionality for now
 	if [ $DEBUG_FLAG == $YES ]
 	then
-		echo "repoFlag : $repoFlag"
 		echo "remoteConfig : $remoteConfig"
-		echo "currentUser : $currentUser"
 		echo "pathToLocalRepo : $pathToLocalRepo"
 		echo "gitFldr : $gitFldr"
 	fi
 
-	if [ -d $pathToLocalRepo ]
+	if [ -d $pathToLocalRepo/$gitFldr ]
 	then
 		cd $pathToLocalRepo/$gitFldr 
 		git pull 
